cd1356d7524c52a7283fd8ae99529ec7033c0e26
yuan-fang-228 <fy0228@gmail.com>
2022-07-19 14:40:05 +1000

    task 7 9 11

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - to get the height of the binary tree
+ * @tree: pointer to the root node
+ * Return: the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right != NULL)
+		right_height = binary_tree_height(tree->right) + 1;
+	if (left_height > right_height)
+		return (left_height);
+	return (right_height);
+}
