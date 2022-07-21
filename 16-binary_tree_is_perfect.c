64caf65ed98a207187d048e735467ae46be93706
Chiara Caprasi <caprasichiara@gmail.com>
2022-07-20 17:53:53 -0700

    fix: checker issue

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -18,8 +18,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (left_height > right_height)
 		return (left_height);
-
-	return (right_height);
+	else
+		return (right_height);
 }
 
 /**
